c9e7a2177011040885a3a31203014e6a1eac657c
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2018-08-23 14:58:01 -0400

    change header info

--- includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/sort_order.php
+++ includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/sort_order.php
@@ -1,19 +1,20 @@
 <?php
-/*
- * sort_order.php
- * @copyright Copyright 2008 - http://www.innov-concept.com
- * @Brand : ClicShopping(Tm) at Inpi all right Reserved
- * @license GPL 2 License & MIT Licencse
-
-*/
+/**
+ *
+ *  @copyright 2008 - https://www.clicshopping.org
+ *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
+ *  @Licence GPL 2 & MIT
+ *  @licence MIT - Portion of osCommerce 2.4
+ *
+ *
+ */
 
   namespace ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\SU\Params;
 
   class sort_order extends \ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\ConfigParamAbstract {
 
-//    public $sort_order = 1000;
     public $default = '1000';
-    public $app_configured = true;
+    public $app_configured = false;
 
     protected function init() {
         $this->title = $this->app->getDef('cfg_surcharge_sort_order_title');
