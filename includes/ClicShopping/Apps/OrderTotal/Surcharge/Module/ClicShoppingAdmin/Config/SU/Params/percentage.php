3a0141b7283d98fd459e61171359442e0385ccb2
ClicShopping - e-commerce Solution <39338579+ClicShopping@users.noreply.github.com>
2019-05-23 19:38:49 -0400

    reformat

--- includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/percentage.php
+++ includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/percentage.php
@@ -1,22 +1,24 @@
 <?php
   /**
- *
- *  @copyright 2008 - https://www.clicshopping.org
- *  @Brand : ClicShopping(Tm) at Inpi all right Reserved
- *  @Licence GPL 2 & MIT
- *  @licence MIT - Portion of osCommerce 2.4
- *  @Info : https://www.clicshopping.org/forum/trademark/
- *
- */
+   *
+   * @copyright 2008 - https://www.clicshopping.org
+   * @Brand : ClicShopping(Tm) at Inpi all right Reserved
+   * @Licence GPL 2 & MIT
+   * @licence MIT - Portion of osCommerce 2.4
+   * @Info : https://www.clicshopping.org/forum/trademark/
+   *
+   */
 
 
   namespace ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\SU\Params;
 
-  class percentage extends \ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\ConfigParamAbstract {
+  class percentage extends \ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
+  {
     public $default = '0';
     public $sort_order = 30;
 
-    protected function init() {
+    protected function init()
+    {
       $this->title = $this->app->getDef('cfg_surcharge_percentage_title');
       $this->description = $this->app->getDef('cfg_surcharge_percentage_description');
     }
