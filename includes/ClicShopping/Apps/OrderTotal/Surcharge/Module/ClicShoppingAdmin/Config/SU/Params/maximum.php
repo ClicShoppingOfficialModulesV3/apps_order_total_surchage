7a28741b712a59daac01824024c58a6e340111c7
ClicShopping <admin@clicshopping.org>
2022-03-04 09:53:01 +0100

    update

--- includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/maximum.php
+++ includes/ClicShopping/Apps/OrderTotal/Surcharge/Module/ClicShoppingAdmin/Config/SU/Params/maximum.php
@@ -15,7 +15,7 @@
   class maximum extends \ClicShopping\Apps\OrderTotal\Surcharge\Module\ClicShoppingAdmin\Config\ConfigParamAbstract
   {
     public $default = '0';
-    public $sort_order = 30;
+    public ?int $sort_order = 30;
 
     protected function init()
     {
